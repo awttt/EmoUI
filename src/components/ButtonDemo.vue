6f28c15b378c0328b78206140f7c0ef99768e5f7
awttt <350944364@qq.com>
2020-09-01 20:51:49 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,10 @@
   </div>
   <h1>示例1</h1>
   <div>
-    <Button>按钮</Button>
+    <Button >按钮</Button>
+    <Button theme='button'>按钮</Button>
+    <Button theme='link'>按钮</Button>
+    <Button theme='text'>按钮</Button>
   </div>
 </template> 
 
@@ -12,6 +15,12 @@
 <script lang='ts'>
 import Button from '../lib/Button.vue'
 export default {
-  components:{Button}
+  components:{Button},
+  setup(){
+    const onClick = ()=>{
+      console.log('hi')
+    }
+    return {onClick}
+  }
 }
 </script>>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,20 @@
 <template>
-    <button>
+<div>
+    <button class='gulu-button'
+    :class="`theme-${theme}`"
+    >
         <slot/>
     </button>
+    </div>
 </template>
+
+<script lang='ts'>
+export default {
+    props:{
+        theme:{
+            type:String,
+            default:'button'
+        }
+    }
+}
+</script>>
